c6e594517c08b1362a5258549c76211ee5081068
CarlosATG <87503410+CarlosATG@users.noreply.github.com>
2021-08-18 14:15:23 -0500

    Con comentarios

--- Palindromo.c
+++ Palindromo.c
@@ -2,15 +2,14 @@
 #include <math.h>
 
 int main(){
-	int h;
-	int i;
-	int j;
-	int z;
-	int x,y;
-	printf ("Bienvenido a tu visualizador de arreglos favorito \nDe cuantoas valores sera tu arreglo\t");
-	scanf("%i", &h);
-	int arreglo[h];
-	int oglerra[h];
+	int h;//Tamaño del arreglo
+	int i;//Variable para ciclo for del arreglo
+	int j;// Variable para el ciclo for del arreglo invertido
+	int x,y,z;//Variables para guardar valor de comparasión de arreglos
+	printf ("Bienvenido a tu visualizador de arreglos favorito \nDe cuantoas valores sera tu arreglo\t");//Interfaz
+	scanf("%i", &h);//Obtenemos tamaño de arreglo
+	int arreglo[h];//Arreglo principal
+	int oglerra[h];//Arreglo invertido
 	
 		for(i=0, j=h-1; i<h ; i++, j--)// ciclo para obtener el valor del arreglo. Mientras que creamos uno con el orden invertido
 		{
@@ -19,27 +18,27 @@ int main(){
 			oglerra[j]=arreglo[i];
 		}
 		
-printf("Los elementos del arreglo son:\n");
+printf("Los elementos del arreglo son:\n");//presebtamos el arreglo en pantalla
 
 for(i=0; i<h; i++)
 	{
 	printf("%i\t", arreglo[i]);
 	}
 	printf("\n");
-for(i=0, j=0; i<h ; i++, j++)
+for(i=0, j=0; i<h ; i++, j++)//Operaciones matemáticas para ver si el arreglo es un palindromo
 		{
-		y=x;
-		x= arreglo[i]-oglerra[j];//x es la resta del ultimo valor del arreglo con el primero
-		z= y+x;
+		y=x;//con esta operación almacenamos el valor de la resta de abajo del ciclo anterior
+		x= arreglo[i]-oglerra[j];//x la resta del primer valor del arreglo principal y el invertido
+		z= y+x;//esta operación nos ayuda a mantener registro de la resta y el valor de la resta anterior, si es un palindromo siempre es 0
 		}
-if (z == 0){//si después de sumar 
+if (z == 0){//Si la suma de todas las restas es 0, h veces, entonces es un palindromo
 		for(j=0; j<h; j++)
 			{
 			printf("%i\t", oglerra[j]);
 			}
 		printf("\nFelicidades, tu vector es un palindromo");
 	}
-else{
+else{//de lo contrario no es un palindromo
 			for(j=0; j<h; j++)
 				{
 				printf("%i\t", oglerra[j]);
